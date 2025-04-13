7df473ad33214d3f0efd2a4f9aa1776ff27a8093
ivayanc <ivanyanchenko.cher@gmail.com>
2025-04-13 21:56:18 +0300

    Add full name

--- app/models/user.py
+++ app/models/user.py
@@ -8,6 +8,7 @@ class User(Base):
 
     id = Column(String, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
+    full_name = Column(String, nullable=True)
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
